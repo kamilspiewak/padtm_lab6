41acb5b23cab46bc166cb28334dc00019ea74258
Kamil <kamilspiewak97@gmail.com>
2020-05-26 12:00:04 +0200

    before gamelist

---
+++ app/src/main/java/com/example/padtm_lab6/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.padtm_lab6;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void gameList(View v)
+    {
+        Intent intencja = new Intent(
+                getApplicationContext(),
+                GameList.class);
+        intencja.putExtra("gra", v.getId());
+        startActivity(intencja);
+    }
+
+}
